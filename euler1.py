ff1a2ec72ad49ed25d7bc9a462fc39bb7be8d2c6
Andrei Tolkachev <nod958fayk@gmail.com>
2025-04-14 23:34:22 +0300

    14_22_40

--- euler1.py
+++ euler1.py
@@ -1,2 +1,40 @@
 import os
-from 
+import tkinter as Tk
+from tkinter import *
+from tkinter import ttk
+
+
+
+
+
+def main():
+    def finish():
+        window.destroy()
+        print('Closed')
+
+
+    window = Tk()
+    
+    window.title('Project Euler')
+    window.geometry('350x200+400+200')
+    
+    
+     
+
+    lbl = Label(window, text=f'Find the sum of all the multiples of 3 or 5 below 1000.')
+    lbl.place(relx=.5, rely=.1, anchor=CENTER)
+    
+    n = sum(n for n in range(1000) if (n%3 == 0 or n%5 == 0))
+  
+
+    lbl1 = Label(window, text=(f'Sum of all numbers: {n}'))
+    lbl1.place(relx=.5, rely=.2, anchor=CENTER)
+
+    btn =ttk.Button(text='Click Me', command=finish)
+    btn.place(relx=.5, rely=.8, anchor=CENTER)
+
+
+    window.mainloop()   
+
+if __name__=="__main__":
+    main()
