320e0eeaffbd75a2b68a308ec7a31aa59df56ad4
Andrei Tolkachev <nod958fayk@gmail.com>
2025-04-15 00:55:46 +0300

    15_00_55

--- euler1.py
+++ euler1.py
@@ -4,37 +4,28 @@ from tkinter import *
 from tkinter import ttk
 
 
-
-
-
 def main():
     def finish():
         window.destroy()
-        print('Closed')
-
+        print("Closed")
 
     window = Tk()
-    
-    window.title('Project Euler')
-    window.geometry('350x200+400+200')
-    
-    
-     
+    window.title("Project Euler")
+    window.geometry("350x200+400+200")
 
-    lbl = Label(window, text=f'Find the sum of all the multiples of 3 or 5 below 1000.')
-    lbl.place(relx=.5, rely=.1, anchor=CENTER)
-    
-    n = sum(n for n in range(1000) if (n%3 == 0 or n%5 == 0))
-  
+    lbl = Label(window, text=f"Find the sum of all the multiples of 3 or 5 below 1000.")
+    lbl.place(relx=0.5, rely=0.1, anchor=CENTER)
 
-    lbl1 = Label(window, text=(f'Sum of all numbers: {n}'))
-    lbl1.place(relx=.5, rely=.2, anchor=CENTER)
+    n = sum(n for n in range(1000) if (n % 3 == 0 or n % 5 == 0))
 
-    btn =ttk.Button(text='Click Me', command=finish)
-    btn.place(relx=.5, rely=.8, anchor=CENTER)
+    lbl1 = Label(window, text=(f"Sum of all numbers: {n}"))
+    lbl1.place(relx=0.5, rely=0.2, anchor=CENTER)
+
+    btn = ttk.Button(text="Click Me", command=finish)
+    btn.place(relx=0.5, rely=0.8, anchor=CENTER)
+
+    window.mainloop()
 
 
-    window.mainloop()   
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
